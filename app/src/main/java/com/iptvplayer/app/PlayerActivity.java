9f42c414d446ed160053b186b2294089e1d108ce
xetya77 <nubuuys77@gmail.com>
2026-03-11 16:53:56 +0800

    Add files via upload

--- app/src/main/java/com/iptvplayer/app/PlayerActivity.java
+++ app/src/main/java/com/iptvplayer/app/PlayerActivity.java
@@ -19,6 +19,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import androidx.annotation.OptIn;
 import androidx.media3.common.C;
